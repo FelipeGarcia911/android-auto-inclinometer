6205e5f0acaeeb2d14086a7845e8a07da5780eac
Felipe Garcia <felipegarcia@Felipes-MacBook-Pro.local>
2025-06-24 17:37:51 -0500

    initial project setup

---
+++ app/src/main/java/com/felipeg/inclinometer4x4/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.felipeg.inclinometer4x4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.felipeg.inclinometer4x4.ui.theme.Inclinometer4x4Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Inclinometer4x4Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Inclinometer4x4Theme {
+        Greeting("Android")
+    }
+}
