a81c3f736e45a2cbb8abd4581870bce4afeff848
Felipe Garcia <felipegarcia@Felipes-MacBook-Pro.local>
2025-07-06 21:39:49 -0500

    feat: add background image

--- app/src/main/java/com/felipeg/inclinometer4x4/presentation/ui/DashboardScreen.kt
+++ app/src/main/java/com/felipeg/inclinometer4x4/presentation/ui/DashboardScreen.kt
@@ -1,5 +1,6 @@
 package com.felipeg.inclinometer4x4.presentation.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -8,8 +9,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.felipeg.inclinometer4x4.R
 import com.felipeg.inclinometer4x4.presentation.ui.component.CombinedInclinometer
 import com.felipeg.inclinometer4x4.presentation.ui.component.GForceMeter
 import com.felipeg.inclinometer4x4.presentation.viewmodel.SensorViewModel
@@ -22,26 +26,34 @@ fun DashboardScreen(
     val gForce by viewModel.gForceState.collectAsState()
     val maxGForce by viewModel.maxGForceState.collectAsState()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        CombinedInclinometer(
-            roll = angle.roll,
-            pitch = angle.pitch,
-            modifier = Modifier.size(300.dp)
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.background_portrait),
+            contentDescription = "Background",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillHeight,
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        Text("Roll: ${angle.roll.toInt()}°, Pitch: ${angle.pitch.toInt()}°", style = MaterialTheme.typography.headlineSmall)
-        Spacer(modifier = Modifier.height(32.dp))
-        GForceMeter(
-            gForceX = gForce.x,
-            gForceY = gForce.y,
-            maxGForce = maxGForce,
-            modifier = Modifier.size(300.dp)
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            CombinedInclinometer(
+                roll = angle.roll,
+                pitch = angle.pitch,
+                modifier = Modifier.size(300.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text("Roll: ${angle.roll.toInt()}°, Pitch: ${angle.pitch.toInt()}°", style = MaterialTheme.typography.headlineSmall)
+            Spacer(modifier = Modifier.height(32.dp))
+            GForceMeter(
+                gForceX = gForce.x,
+                gForceY = gForce.y,
+                maxGForce = maxGForce,
+                modifier = Modifier.size(300.dp)
+            )
+        }
     }
 }
